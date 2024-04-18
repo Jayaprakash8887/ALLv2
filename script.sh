61f98fc4cab1e79b7277f46b40b51db2c7a1d60c
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2024-04-18 11:44:50 +0530

    API structure skeleton

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-/opt/conda/bin/uvicorn main:app --host 0.0.0.0 --port 8022 --timeout-keep-alive 600 --workers 4
+/opt/conda/bin/uvicorn main:app --host 0.0.0.0 --port 8022 --root-path /all_bot --timeout-keep-alive 600 --workers 4
 tail -f /dev/null
